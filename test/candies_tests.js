cb159c4f819e6956d1411c19e76b57cb031832ea
Evon <fumblies@gmail.com>
2016-07-11 12:25:11 +0800

    updates app.js for test deployment

--- test/candies_tests.js
+++ test/candies_tests.js
@@ -3,6 +3,8 @@
 const expect = require('chai').expect
 const supertest = require('supertest')
 const api = supertest('http://localhost:3000')
+const app = require('../app')
+
 
 describe('GET /candies', () => {
   it('should return a 200 response', (done) => {
